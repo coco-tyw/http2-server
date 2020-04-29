607e82b910bd5a81422925ab19f54066af0bf39b
coco <mrkokipro@icloud.com>
2020-04-30 00:11:48 +0900

    http2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
+import spdy from 'spdy'
+
+const port = 3000
+const option: spdy.server.ServerOptions = {
+  key: fs.readFileSync(path.join(__dirname, './keys/private.key')),
+  cert: fs.readFileSync(path.join(__dirname, '/keys/server.crt')),
+  passphrase: 'password'
+}
 
 const app = express()
-
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+spdy.createServer(option, app).listen(port)
